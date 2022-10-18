8b78d04472db9015ee160ba4ae5c3feb7e8acf67
jjcrl <j412cff@icloud.com>
2022-10-18 15:46:36 +0100

    fixed bug

--- src/features/main-form/MonitoringSections.js
+++ src/features/main-form/MonitoringSections.js
@@ -2,24 +2,26 @@ import { TextField, Select, MenuItem } from "@mui/material";
 import styles from "./Form.module.css";
 
 export function MonitoringSection() {
-  <div className={styles.container}>
-    <h2>Monitoring Form</h2>
-    <div className={styles.options_container}>
-      <TextField variant="outlined" label="area" />
+  return (
+    <div className={styles.container}>
+      <h2>Monitoring Form</h2>
+      <div className={styles.options_container}>
+        <TextField variant="outlined" label="area" />
 
-      <Select variant="outlined" label="accom">
-        <MenuItem>NFA</MenuItem>
-        <MenuItem>Soft Surfing</MenuItem>
-        <MenuItem>ABEN</MenuItem>
-        <MenuItem>Private Rented</MenuItem>
-        <MenuItem>Housing Assoc</MenuItem>
-      </Select>
+        <Select variant="outlined" label="accom">
+          <MenuItem>NFA</MenuItem>
+          <MenuItem>Soft Surfing</MenuItem>
+          <MenuItem>ABEN</MenuItem>
+          <MenuItem>Private Rented</MenuItem>
+          <MenuItem>Housing Assoc</MenuItem>
+        </Select>
 
-      <TextField variant="outlined" label="Type of sex work" />
-      <TextField variant="outlined" label="Nationality" />
-      <TextField variant="outlined" label="Ethnicity" />
-      <TextField variant="outlined" label="Gender" />
+        <TextField variant="outlined" label="Type of sex work" />
+        <TextField variant="outlined" label="Nationality" />
+        <TextField variant="outlined" label="Ethnicity" />
+        <TextField variant="outlined" label="Gender" />
+      </div>
     </div>
-  </div>;
+  );
 }
 export default MonitoringSection;
